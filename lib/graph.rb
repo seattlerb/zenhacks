b64cab4ff05baaaf0cb6f03866775c7ccacd7788
Ryan Davis <ryand@zenspider.com>
2005-05-24 20:39:03 -0800

    Updated. Yay!
    [git-p4: depot-paths = "//src/ZenHacks/dev/": change = 2071]

--- lib/graph.rb
+++ lib/graph.rb
@@ -5,11 +5,13 @@ class Graph < Hash
   attr_reader :attribs
   attr_reader :prefix
   attr_reader :order
-  def initialize(name="a_graph")
-    super() { |h,k| h[k] = [] }
-    @name = name
+  attr_reader :edge
+
+  def initialize
+    super { |h,k| h[k] = [] }
     @prefix = []
     @attribs = Hash.new { |h,k| h[k] = [] }
+    @edge = Hash.new { |h,k| h[k] = Hash.new { |h2,k2| h2[k2] = [] } }
     @order = []
   end
 
@@ -25,7 +27,7 @@ class Graph < Hash
       end
     end
   end
-
+  
   def invert
     result = self.class.new
     each_pair do |from, to|
@@ -41,14 +43,14 @@ class Graph < Hash
     end
     result
   end
-
+  
   def keys_by_count
     counts.sort_by { |x,y| y }.map {|x| x.first }
   end
 
   def to_s
     result = []
-    result << "digraph #{@name}"
+    result << "digraph absent"
     result << "  {"
     @prefix.each do |line|
       result << line
@@ -57,10 +59,20 @@ class Graph < Hash
       result << "    #{node.inspect} [ #{attribs.join(',')} ]"
     end
     each_pair do |from, to|
-      result << "    #{from.inspect} -> #{to.inspect};"
+      edge = @edge[from][to].join(", ")
+      edge = " [ #{edge} ]" unless edge.empty?
+      result << "    #{from.inspect} -> #{to.inspect}#{edge};"
     end
     result << "  }"
     result.join("\n")
   end
 
+  def save(path, type="png")
+    File.open(path + ".dot", "w") do |f|
+      f.puts self.to_s
+      f.flush
+      cmd = "/usr/local/bin/dot -T#{type} #{path}.dot > #{path}.#{type}"
+      system cmd
+    end
+  end
 end
