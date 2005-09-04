b415339586d2f13cf8983f524cf60438bae3fad3
Ryan Davis <ryand@zenspider.com>
2005-09-04 01:47:48 -0800

    Quite a few changes to presentation, mostly for maintenence. Cleaned some of the timing stuff, but not much. I still think there is a problem with self-seconds column data,   from the C side, esp with small sample sizes.
    [git-p4: depot-paths = "//src/ZenHacks/dev/": change = 2175]

--- lib/zenprofile.rb
+++ lib/zenprofile.rb
@@ -11,6 +11,7 @@ class ZenProfiler
 
   def self.start_profile
     @@start = self.instance.time_now
+    @@start2 = Process.times[0]
     @@stack = [[0, 0, [nil, :toplevel]], [0, 0, [nil, :dummy]]]
     @@map = {"#toplevel" => [1, 0.0, 0.0, [nil, "#toplevel"]]}
     self.instance.add_event_hook
@@ -22,30 +23,43 @@ class ZenProfiler
 
   def self.print_profile(f)
     stop_profile
-    total = self.instance.time_now - @@start
-    if total == 0 then total = 0.01 end
-    @@map["#toplevel"][1] = total
-    data = @@map.values
-    data.sort!{|a,b| b[2] <=> a[2]} # TODO: change to sort_by
+    @@total = self.instance.time_now - @@start
+    @@total = 0.01 if @@total == 0
+    @@total2 = Process.times[0] - @@start2
+    @@map["#toplevel"][1] = @@total
+    data = @@map.values.sort_by { |vals| -vals[2] }
     sum = 0
 
-    f.printf "  %%   cumulative   self              self     total\n"           
-    f.printf " time   seconds   seconds    calls  ms/call  ms/call  name\n"
-    for d in data
-      sum += d[2]
-      klass = d[3].first
-      meth  = d[3].last.to_s
+    p data
 
-      signature = if klass.nil?
-                    meth
-                  elsif klass.kind_of?(Class)
-                    klass.to_s.sub(/#<\S+:(\S+)>/, '\\1') + "#" + meth
-                  else
-                    klass.class.name + "." + meth
-                  end
+    f.puts "Total time = %f" % @@total
+    f.puts "Total time = %f" % @@total2
+    f.puts
+    f.puts "          total     self              self    total"
+    f.puts "% time  seconds  seconds    calls  ms/call  ms/call  name"
 
-      f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/total*100, sum, d[2], d[0]
-      f.printf "%8.2f %8.2f  %s\n", d[2]*1000/d[0], d[1]*1000/d[0], signature
+    data.each do |calls, total_ms, self_ms, name|
+      sum += self_ms
+      klass = name.first
+      meth  = name.last.to_s
+
+      signature =
+        if klass.nil?
+          meth
+        elsif klass.kind_of?(Class)
+          klass.to_s.sub(/#<\S+:(\S+)>/, '\\1') + "#" + meth
+        else
+          klass.class.name + "." + meth
+        end
+      
+      f.printf "%6.2f ",  (self_ms / @@total * 100.0)
+      f.printf "%8.2f ", sum
+      f.printf "%8.2f ",  self_ms
+      f.printf "%8d ",    calls
+      f.printf "%8.2f ",  (self_ms * 1000.0 / calls)
+      f.printf "%8.2f ",  (total_ms * 1000.0 / calls)
+      f.printf "%s",      signature
+      f.puts
     end
   end
 
@@ -100,10 +114,10 @@ static VALUE map = Qnil;
           signature = rb_ary_new2(2);
           rb_ary_store(signature, 0, klass);
           rb_ary_store(signature, 1, ID2SYM(mid));
-          VALUE time = rb_ary_new();
-          rb_ary_push(time, time_now());
-          rb_ary_push(time, rb_float_new(0.0));
-          rb_ary_push(time, signature);
+          VALUE time = rb_ary_new2(3);
+          rb_ary_store(time, 0, time_now());
+          rb_ary_store(time, 1, rb_float_new(0.0));
+          rb_ary_store(time, 2, signature);
           rb_ary_push(stack, time);
         }
         break;
@@ -113,16 +127,18 @@ static VALUE map = Qnil;
         VALUE now = time_now();
         VALUE tick = rb_ary_pop(stack);
 
+        if (!RTEST(tick)) break;
+
         VALUE signature = rb_ary_entry(tick, -1);
         
         VALUE data = Qnil;
         st_lookup(RHASH(map)->tbl, signature, &data);
         if (NIL_P(data)) {
-          data = rb_ary_new();
-          rb_ary_push(data, INT2FIX(0));
-          rb_ary_push(data, rb_float_new(0.0));
-          rb_ary_push(data, rb_float_new(0.0));
-          rb_ary_push(data, signature);
+          data = rb_ary_new2(4);
+          rb_ary_store(data, 0, INT2FIX(0));
+          rb_ary_store(data, 1, rb_float_new(0.0));
+          rb_ary_store(data, 2, rb_float_new(0.0));
+          rb_ary_store(data, 3, signature);
           rb_hash_aset(map, signature, data);
         }
 
@@ -132,6 +148,8 @@ static VALUE map = Qnil;
 
         rb_ary_store(data, 1, rb_float_new(NUM2DBL(rb_ary_entry(data, 1))
                                            + cost));
+
+        // data[2] += cost - tick[1]
         rb_ary_store(data, 2, rb_float_new(NUM2DBL(rb_ary_entry(data, 2))
                                            + cost
                                            - NUM2DBL(rb_ary_entry(tick, 1))));
