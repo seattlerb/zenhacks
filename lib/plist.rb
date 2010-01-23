0246fed8c99cf6cd91b3b5640a7ad725536fdc2d
Ryan Davis <ryand@zenspider.com>
2010-01-22 17:36:23 -0800

    Refactored to Plist.parse [git-p4: depot-paths = "//src/ZenHacks/dev/": change = 5620]

--- lib/plist.rb
+++ lib/plist.rb
@@ -2,46 +2,47 @@ require 'rexml/parsers/pullparser'
 require 'time'
 require 'pp'
 
-def parse_plist(path)
-  f = File.open(path)
-  stack = []
-  data = []
-  parser = REXML::Parsers::PullParser.new(f)
-  parser.each do |res|
-    raise res[1] if res.error?
-    tag = res[0]
-    case res.event_type
-    when :start_element
-      stack.push [tag]
-    when :text
-      data.push tag
-    when :end_element
-      raise "Um. not the same #{stack.last} vs #{tag}" if
-        stack.last.first != tag
+class Plist
+  def self.parse(path)
+    f = File.open(path)
+    stack = []
+    data = []
+    parser = REXML::Parsers::PullParser.new(f)
+    parser.each do |res|
+      raise res[1] if res.error?
+      tag = res[0]
+      case res.event_type
+      when :start_element
+        stack.push [tag]
+      when :text
+        data.push tag
+      when :end_element
+        raise "Um. not the same #{stack.last} vs #{tag}" if
+          stack.last.first != tag
 
-      last = stack.pop
+        last = stack.pop
 
-      case tag
-      when "key", "string" then
-        stack.last.push data.join.strip
-      when "date" then
-        stack.last.push Time.parse(data.join)
-      when "true" then
-        stack.last.push true
-      when "integer" then
-        stack.last.push data.join.strip.to_i
-      when "dict" then
-        stack.last.push Hash[*last[1..-1]]
-      when "array" then
-        stack.last.push last[1..-1]
-      when "plist" then
-        return last.last
-      else
-        raise "unhandled type #{tag.inspect}"
+        case tag
+        when "key", "string" then
+          stack.last.push data.join.strip
+        when "date" then
+          stack.last.push Time.parse(data.join)
+        when "true" then
+          stack.last.push true
+        when "integer" then
+          stack.last.push data.join.strip.to_i
+        when "dict" then
+          stack.last.push Hash[*last[1..-1]]
+        when "array" then
+          stack.last.push last[1..-1]
+        when "plist" then
+          return last.last
+        else
+          raise "unhandled type #{tag.inspect}"
+        end
+
+        data = []
       end
-
-      data = []
     end
   end
 end
-
